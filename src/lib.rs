d20d00e6dbdf156e6461397ca0772f9fe79fa477
Albert Chung <albertswchung@gmail.com>
2022-06-23 22:17:27 +0100

    Define operations for arithmetic.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
-#[derive(Debug,PartialEq)]
+use std::ops::{Add, Sub};
+
+#[derive(Debug, PartialEq)]
 pub struct Tuple {
     pub x: f32,
     pub y: f32,
@@ -7,8 +9,8 @@ pub struct Tuple {
 }
 
 impl Tuple {
-    pub fn new(x: f32, y:f32, z:f32, w:f32) -> Self {
-        Self {x, y, z, w}
+    pub fn new(x: f32, y: f32, z: f32, w: f32) -> Self {
+        Self { x, y, z, w }
     }
 
     pub fn is_vector(&self) -> bool {
@@ -20,8 +22,40 @@ impl Tuple {
     }
 }
 
+impl Add for Tuple {
+    type Output = Self;
 
-pub fn point(x: f32, y: f32, z:f32) -> Tuple {
+    fn add(self, other: Self) -> Self {
+        if self.w + other.w == 2.0 {
+            panic!("Cannot add point to a point")
+        } else {
+            Self {
+                x: self.x + other.x,
+                y: self.y + other.y,
+                z: self.z + other.z,
+                w: self.w + other.w,
+            }
+        }
+    }
+}
+
+impl Sub for Tuple {
+    type Output = Self;
+
+    fn sub(self, other: Self) -> Self {
+        if self.w == 0. && other.w == 1. {
+            panic!("Cannot subtract point from vector")
+        }
+        Tuple {
+            x: self.x - other.x,
+            y: self.y - other.y,
+            z: self.z - other.z,
+            w: self.w - other.w,
+        }
+    }
+}
+
+pub fn point(x: f32, y: f32, z: f32) -> Tuple {
     Tuple::new(x, y, z, 1.0)
 }
 
--- tests/test_features.rs
+++ tests/test_features.rs
@@ -15,12 +15,12 @@ fn test_vector_raw() {
 
 #[test]
 fn test_point_raw() {
-    let a = Tuple::new(4.3, -4.2, 3.1, 0.0);
+    let a = Tuple::new(4.3, -4.2, 3.1, 0.);
 
     assert_eq!(a.x, 4.3);
     assert_eq!(a.y, -4.2);
     assert_eq!(a.z, 3.1);
-    assert_eq!(a.w, 0.0);
+    assert_eq!(a.w, 0.);
 
     assert!(a.is_point());
     assert_eq!(a.is_vector(), false);
@@ -34,6 +34,42 @@ fn test_point() {
 
 #[test]
 fn test_vector() {
-    let p: Tuple = vector(4.0, -4.0, 3.0);
+    let p: Tuple = vector(4., -4., 3.);
     assert_eq!(p, Tuple::new(4., -4., 3., 0.));
+}
+
+#[test]
+fn test_add_tuple() {
+    let a1 = Tuple::new(3., -2., 5., 1.);
+    let a2: Tuple = Tuple::new(-2., 3.0, 1., 0.);
+
+    let sum = a1 + a2;
+    assert_eq!(sum, Tuple::new(1.0, 1.0, 6.0, 1.0));
+}
+
+#[test]
+fn test_sub_points() {
+    let p1 = point(3., 2., 1.);
+    let p2 = point(5., 6., 7.);
+
+    let diff = p1 - p2;
+    assert_eq!(diff, vector(-2., -4., -6.))
+}
+
+#[test]
+fn sub_vec_from_point() {
+    let p = point(3., 2., 1.);
+    let v = vector(5., 6., 7.);
+
+    let diff = p - v;
+    assert_eq!(diff, point(-2., -4., -6.))
+}
+
+#[test]
+fn sub_vec() {
+    let v1 = vector(3., 2., 1.);
+    let v2 = vector(5., 6., 7.);
+    let diff = v1 - v2;
+
+    assert_eq!(diff, vector(-2., -4., -6.))
 }
