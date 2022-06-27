aabc71f9cd7ab620284552a927bb6ac74d420d0a
Albert Chung <albertswchung@gmail.com>
2022-06-27 23:05:17 +0100

    Add some tests for serializing PPM to string.

--- src/canvas.rs
+++ src/canvas.rs
@@ -1,5 +1,7 @@
 use crate::Color;
 
+const MAX_PPM_LEN: usize = 70;
+
 #[derive(Debug,Clone)]
 pub struct Canvas {
     pub height: u32,
@@ -18,10 +20,44 @@ impl Canvas {
     }
 }
 
-pub fn write_pixel(canvas: &mut Canvas, row: usize, col: usize, color: Color) {
-    canvas.pixels[row][col] = color;
+pub fn write_pixel(canvas: &mut Canvas, x: usize, y: usize, color: Color) {
+    canvas.pixels[y][x] = color;
 } 
 
-pub fn pixel_at(canvas: &Canvas, row: usize, col: usize) -> Color {
-    canvas.pixels[row][col]
+pub fn pixel_at(canvas: &Canvas, x: usize, y: usize) -> Color {
+    canvas.pixels[y][x]
+}
+
+pub fn canvas_to_ppm(canvas: &Canvas) -> Vec<String> {
+    let mut result = Vec::new();
+
+    // Define header
+    result.push(String::from("P3"));
+    result.push(format!("{} {}", canvas.height, canvas.width).to_string());
+    result.push(String::from("255"));
+
+    for row in &canvas.pixels {
+        let mut row_txt = String::new();
+        for pix in row {
+
+            let red_u8 = (pix.red.clamp(0.0, 1.0) * 255.) as u8;
+            let green_u8 = (pix.green.clamp(0.0, 1.0) * 255.) as u8;
+            let blue_u8 = (pix.blue.clamp(0.0, 1.0) * 255.) as u8;
+
+            let rgb = format!("{} {} {}", red_u8, green_u8, blue_u8);
+            if row_txt.len() + rgb.len() + 1 > MAX_PPM_LEN {
+                result.push(row_txt.to_string());
+                row_txt = String::new();
+            } else {
+                if row_txt.len() > 0 {
+                    row_txt.push_str(" ");
+                }
+                row_txt.push_str(&rgb);
+            }
+        }
+        if row_txt.len() > 0 {
+            result.push(row_txt.to_string());
+        }
+    }
+    result
 }
--- tests/test_canvas.rs
+++ tests/test_canvas.rs
@@ -1,7 +1,5 @@
 mod tests {
-    use std::fmt::write;
-
-    use tracer::Color;
+    use tracer::{Color};
     use tracer::canvas::{Canvas, write_pixel, pixel_at, canvas_to_ppm};
 
     #[test]
@@ -29,21 +27,21 @@ mod tests {
     }
 
     #[test]
-    fn canvas_to_ppm_header() {
+    fn test_canvas_to_ppm_header() {
         let c = Canvas::new(5, 3);
-        let ppm = canvas_to_ppm(c);
-        let expected = "
-        P3
-        5 3
-        255
-        ";
+        let ppm = canvas_to_ppm(&c);
+        let expected = vec![
+            String::from("P3"),
+            String::from("5 3"),
+            String::from("255"),
+        ];
 
-        assert_eq!(ppm, expected)
+        assert_eq!(ppm[0..3], expected)
     }
 
     #[test]
-    fn canvas_to_ppm_pixels() {
-        let c = Canvas::new(5, 3);
+    fn test_canvas_to_ppm_pixels() {
+        let mut c = Canvas::new(5, 3);
         let c1 = Color::new(1.5, 0.0, 0.0);
         let c2 = Color::new(0.0, 0.5, 0.0);
         let c3 = Color::new(-0.5, 0.0, 1.0);
@@ -51,10 +49,34 @@ mod tests {
         write_pixel(&mut c, 2, 1, c2);
         write_pixel(&mut c, 4, 2, c3);
 
-        let expected = "
-        255 0 0 0 0 0 0 0 0 0 0 0 0 0 0
-        0 0 0 0 0 0 0 128 0 0 0 0 0 0 0
-        0 0 0 0 0 0 0 0 0 0 0 0 0 0 255
-        ";
+        let expected = vec![
+            String::from("255 0 0 0 0 0 0 0 0 0 0 0 0 0 0"),
+            String::from("0 0 0 0 0 0 0 128 0 0 0 0 0 0 0"),
+            String::from("0 0 0 0 0 0 0 0 0 0 0 0 0 0 255"),
+        ];
+        
+        let ppm = canvas_to_ppm(&c);
+        assert_eq!(ppm[3..5], expected)
     }
+
+    #[test]
+    fn test_ppm_line_overflow() {
+        let mut c = Canvas::new(10, 2);
+        for i in 0..10 {
+            for j in 0..2 {
+                write_pixel(&mut c, i, j, Color::new(1.0, 0.8, 0.6))
+            }
+        }
+
+        let expected = vec![
+            String::from("255 204 153 255 204 153 255 204 153 255 204 153 255 204 153 255 204"),
+            String::from("153 255 204 153 255 204 153 255 204 153 255 204 153"),
+            String::from("255 204 153 255 204 153 255 204 153 255 204 153 255 204 153 255 204"),
+            String::from("153 255 204 153 255 204 153 255 204 153 255 204 153"),
+        ];
+
+        let ppm = canvas_to_ppm(&c);
+        assert_eq!(ppm, expected)
+    }
+
 }
