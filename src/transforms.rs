1cd493808bd72ef4c35c60caa20e515b4ce2b350
Albert Chung <albertswchung@gmail.com>
2022-07-09 19:57:57 +0100

    Add rotations with tests.

--- src/transforms.rs
+++ src/transforms.rs
@@ -9,3 +9,44 @@ pub fn translation(x: f32, y: f32, z: f32) -> Matrix<f32, 4, 4> {
     ]);
     trans_mat
 }
+
+pub fn scaling(x: f32, y: f32, z: f32) -> Matrix<f32, 4, 4> {
+    let scale_mat = Matrix::new([
+        [x, 0., 0., 0.],
+        [0., y, 0., 0.],
+        [0., 0., z, 0.],
+        [0., 0., 0., 1.],
+    ]);
+    scale_mat
+}
+
+
+pub fn rotation_x(r: f32) -> Matrix<f32, 4, 4> {
+    let rot_matrix_x = Matrix::new([
+        [1., 0., 0., 0.],
+        [0., f32::cos(r), -f32::sin(r), 0.],
+        [0., f32::sin(r), f32::cos(r), 0.],
+        [0., 0., 0., 1.],
+    ]);
+    rot_matrix_x
+}
+
+pub fn rotation_y(r: f32) -> Matrix<f32, 4, 4> {
+    let rot_matrix_y = Matrix::new([
+        [f32::cos(r), 0., f32::sin(r), 0.],
+        [0., 1., 0., 0.],
+        [-f32::sin(r), 0., f32::cos(r), 0.],
+        [0., 0., 0., 1.],
+    ]);
+    rot_matrix_y
+}
+
+pub fn rotation_z(r: f32) -> Matrix<f32, 4, 4> {
+    let rot_matrix_z = Matrix::new([
+        [f32::cos(r), -f32::sin(r), 0., 0.],
+        [f32::sin(r), f32::cos(r), 0., 0.],
+        [0., 0., 1., 0.],
+        [0., 0., 0., 1.],
+    ]);
+    rot_matrix_z
+}
--- tests/test_transformations.rs
+++ tests/test_transformations.rs
@@ -1,12 +1,12 @@
 mod tests {
-    use tracer::{point, vector};
-    use tracer::transforms::translation;
+    use std::f32::consts::PI;
+    use tracer::{point, vector, magnitude};
+    use tracer::transforms::{translation, scaling, rotation_x, rotation_y, rotation_z};
 
     #[test]
     fn test_translation() {
         let p = point(-3., 4., 5.);
         let transform = translation(5., -3., 2.);
-
         let expected = point(2., 1., 7.);
         assert_eq!(transform * p, expected)
     }
@@ -16,7 +16,6 @@ mod tests {
         let transform = translation(5., -3., 2.);
         let inv = transform.inverse()?;
         let p = point(-3., 4., 5.);
-
         let expected = point(-8., 7., 3.);
         assert_eq!(inv * p, expected);
         Ok(())
@@ -26,7 +25,90 @@ mod tests {
     fn test_transform_leaves_vectors() {
         let transform = translation(5., -3., 2.);
         let v = vector(-3., 4., 5.);
-
         assert_eq!(transform * v, v)
+    }
+
+    #[test]
+    fn test_scaling_point() {
+        let transform = scaling(2., 3., 4.);
+        let p = point(-4., 6., 8.);
+        let expected = point(-8., 18., 32.);
+        assert_eq!(transform * p, expected)
+    }
+
+    #[test]
+    fn test_scaling_vector() {
+        let transform = scaling(2., 3., 4.);
+        let v = vector(-4., 6., 8.);
+        let expected = vector(-8., 18., 32.);
+        assert_eq!(transform * v, expected)
+    }
+
+    #[test]
+    fn test_scaling_inverse() -> Result<(), &'static str> {
+        let transform = scaling(2., 3., 4.);
+        let inv = transform.inverse()?;
+        let v = vector(-4., 6., 8.);
+        let expected = vector(-2., 2., 2.);
+        assert_eq!(inv * v, expected);
+        Ok(())
+    }
+
+    #[test]
+    fn test_reflection() {
+        let transform = scaling(-1., 1., 1.);
+        let p = point(2., 3., 4.);
+        let expected = point(-2., 3., 4.);
+        assert_eq!(transform * p, expected)
+    }
+
+    #[test]
+    fn test_rotation_x() {
+        let p = point(0., 1., 0.);
+        let half_quarter = rotation_x(PI / 4.);
+        let full_quarter = rotation_x(PI / 2.);
+        let expected_half = point(0., f32::sqrt(2.) / 2., f32::sqrt(2.) / 2.);
+        let expected_full = point(0., 0., 1.);
+        let diff_half = half_quarter * p - expected_half;
+        let diff_full = full_quarter * p - expected_full;
+        assert!(magnitude(diff_half) < 1e-5);
+        assert!(magnitude(diff_full) < 1e-5);
+    }
+
+    #[test]
+    fn test_inverse_rotation() -> Result<(), &'static str> {
+        let p = point(0., 1., 0.);
+        let half_quarter = rotation_x(PI / 4.);
+        let inv = half_quarter.inverse()?;
+        let expected = point(0., f32::sqrt(2.) / 2., -f32::sqrt(2.) / 2.);
+        let diff = inv * p - expected; 
+        assert!(magnitude(diff) < 1e-5);
+        Ok(())
+    }
+
+    #[test]
+    fn test_rotation_y() {
+        let p = point(0., 0., 1.);
+        let half_quarter = rotation_y(PI / 4.);
+        let full_quarter = rotation_y(PI / 2.);
+        let expected_half = point(f32::sqrt(2.) / 2., 0., f32::sqrt(2.) / 2.);
+        let expected_full = point(1., 0., 0.);
+        let diff_half = half_quarter * p - expected_half;
+        let diff_full = full_quarter * p - expected_full;
+        assert!(magnitude(diff_half) < 1e-5);
+        assert!(magnitude(diff_full) < 1e-5);
+    }
+
+    #[test]
+    fn test_rotation_z() {
+        let p = point(0., 1., 0.);
+        let half_quarter = rotation_z(PI / 4.);
+        let full_quarter = rotation_z(PI / 2.);
+        let expected_half = point(-f32::sqrt(2.) / 2., f32::sqrt(2.) / 2., 0.);
+        let expected_full = point(-1., 0., 0.);
+        let diff_half = half_quarter * p - expected_half;
+        let diff_full = full_quarter * p - expected_full;
+        assert!(magnitude(diff_half) < 1e-5);
+        assert!(magnitude(diff_full) < 1e-5);
     }
 }
