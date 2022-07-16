0ed7871b70ac0f95a1cc40a3ea92a308c5518e17
Albert Chung <albertswchung@gmail.com>
2022-07-16 11:31:26 +0100

    Fix typo.

--- tests/test_spheres.rs
+++ tests/test_spheres.rs
@@ -104,7 +104,7 @@ mod tests {
 
     #[test]
     fn test_normal_y() {
-        s = Sphere::new();
+        let s = Sphere::new();
         let n = normal_at(s, point(0., 1., 0.));
         assert_eq!(n, vector(0., 1., 0.));
     }
