b9e0932126b98a9d7da6dafab84f1dbb58698d6f
Albert Chung <albertswchung@gmail.com>
2022-10-16 10:17:34 +0100

    Update main example with transparency.

--- src/main.rs
+++ src/main.rs
@@ -8,14 +8,18 @@ use tracer::lights::PointLight;
 use tracer::materials::Material;
 use tracer::patterns::GradientPattern;
 use tracer::patterns::RadialGradient;
+use tracer::patterns::StripePattern;
 use tracer::patterns::{CheckerPattern, Pattern, PatternType, RingPattern};
 use tracer::plane::Plane;
 use tracer::sphere::Sphere;
+use tracer::transforms::rotation_z;
 use tracer::transforms::{rotation_x, scaling, shearing, translation, view_transform};
 use tracer::world::{ShapeEnum, World};
 use tracer::{point, vector, Color};
 
 fn main() -> Result<(), &'static str> {
+
+    // Middle Sphere
     let mut middle = Sphere::default();
     middle.transform = translation(-0.5, 1., 0.5);
     middle.material = Material::default();
@@ -24,7 +28,7 @@ fn main() -> Result<(), &'static str> {
     middle.material.specular = 0.3;
     middle.material.reflective = 1.;
 
-    let stripe = PatternType::Radial(RadialGradient::new(
+    let stripe = PatternType::Stripe(StripePattern::new(
         Color::new(1.0, 0.5, 0.0),
         Color::new(0., 0.5, 0.5),
     ));
@@ -34,6 +38,27 @@ fn main() -> Result<(), &'static str> {
     middle.material.transparency = 0.9;
     middle.material.reflective = 0.9;
 
+    // Interior Sphere
+    let mut interior = Sphere::default();
+    interior.transform = translation(-0.5, 1., 1.5) * scaling(0.5, 0.5, 0.5);
+    interior.material = Material::default();
+    interior.material.color = Color::new(0., 0.5, 0.8);
+    interior.material.reflective = 0.9;
+    interior.material.transparency = 0.2;
+    
+    let checker = PatternType::Checker(CheckerPattern::new(
+        Color::new(0., 1.0, 0.5),
+        Color::new(0.5, 0., 0.9)
+    ));
+    let mut checker_pattern = Pattern::new(checker);
+    checker_pattern.set_transform(
+        rotation_x(PI / 4.) 
+        * rotation_z(PI / 4.)
+        * scaling(0.2, 0.5, 2.0)
+    );
+    interior.material.pattern = Some(checker_pattern);
+
+    // Left Sphere
     let mut left = Sphere::default();
     left.transform = translation(-1., 0., -1.75)
         * scaling(0.5, 0.5, 0.5)
@@ -58,12 +83,14 @@ fn main() -> Result<(), &'static str> {
         Color::new(0.0, 0.0, 0.0),
     ));
     floor.material.pattern = Some(Pattern::new(checker));
+    floor.material.reflective = 0.9;
 
     let mut world = World::default();
     world.objects = vec![
         ShapeEnum::Sphere(middle),
         ShapeEnum::Sphere(left),
         ShapeEnum::Plane(floor),
+        ShapeEnum::Sphere(interior),
     ];
     world.light = Some(PointLight::new(
         point(-10., 10., -10.),
